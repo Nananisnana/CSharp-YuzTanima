80ab15dbad96287ea543ada65e554c9218413957
nananisnana <nisapaydin004@gmail.com>
2025-11-01 23:33:23 +0300

    Proje dosyası ekle.

---
+++ yüztanima/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace YuzTanimaProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picKamera = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picKamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picKamera
+            // 
+            this.picKamera.Location = new System.Drawing.Point(12, 12);
+            this.picKamera.Name = "picKamera";
+            this.picKamera.Size = new System.Drawing.Size(550, 426);
+            this.picKamera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picKamera.TabIndex = 0;
+            this.picKamera.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 33;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picKamera);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picKamera)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picKamera;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ yüztanima/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing; 
+using System.Windows.Forms;
+
+
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace YuzTanimaProjesi 
+{
+    public partial class Form1 : Form
+    {
+        
+        VideoCapture capture;
+        Mat frame;            
+
+        
+        Mat gray; 
+        CascadeClassifier cascade; 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            capture = new VideoCapture(0);
+
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("Kamera açılamadı!");
+                return;
+            }
+
+           
+            frame = new Mat();
+            gray = new Mat();
+
+            
+            
+            string cascadePath = System.IO.Path.Combine(Application.StartupPath, "haarcascade_frontalface_default.xml");
+
+            if (!System.IO.File.Exists(cascadePath))
+            {
+                MessageBox.Show("Haar Cascade dosyası bulunamadı! 'haarcascade_frontalface_default.xml' dosyasının özelliklerini kontrol edin.");
+                return;
+            }
+
+            cascade = new CascadeClassifier(cascadePath);
+
+            
+            timer1.Start();
+        }
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            capture.Read(frame);
+
+           
+            if (frame.Empty())
+            {
+                timer1.Stop();
+                return;
+            }
+
+            
+
+            
+            Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+
+            
+            Cv2.EqualizeHist(gray, gray);
+
+          
+            Rect[] faces = cascade.DetectMultiScale(
+                image: gray,
+                scaleFactor: 1.1,
+                minNeighbors: 5,
+                flags: HaarDetectionTypes.ScaleImage,
+                minSize: new OpenCvSharp.Size(30, 30));
+
+            foreach (Rect face in faces)
+            {
+                Cv2.Rectangle(frame, face, Scalar.Red, 3); 
+            }
+
+            
+            picKamera.Image = BitmapConverter.ToBitmap(frame);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            timer1.Stop();
+
+           
+            if (capture != null)
+                capture.Release();
+
+            if (frame != null)
+                frame.Dispose();
+
+            if (gray != null)
+                gray.Dispose();
+
+            if (cascade != null)
+                cascade.Dispose();
+        }
+    }
+}
